4e1a788ab1b4b6ba002105fe2c0f722ca4a67a04
TarMan <taremans@gmail.com>
2022-08-03 20:07:39 +0300

    Add heloooooo

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
   render() {
     return (
@@ -12,6 +13,7 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+		Helloooooooooooooooo
         </p>
       </div>
     );
